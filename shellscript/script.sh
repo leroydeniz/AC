8c4a4d3fd85d9c2a3878f91a9d821696353a7348
Leroy Deniz <61316853+leroydeniz@users.noreply.github.com>
2021-04-23 11:36:48 +0200

    Add files via upload

---
+++ shellscript/script.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#BUCLE PARA RECORRER LAS TRES IMAGENES
+for imagen in Libro1.pgm Libro2.pgm Libro3.pgm
+do
+        #!BUCLE PARA VARIAR LA PLANIFICACION
+        for plan in static dynamic
+        do
+                #BUCLE PARA VARIAR EL NUMERO DE INSTRUCCIONES
+                for instrucciones in 0 1 2 4 8 16 32
+                do
+                        #BUCLE PARA VARIAR EL NUMERO DE HILOS
+                        for hilos in 2 4 8 16 24 32
+                        do
+                                echo
+                                echo "******************************** ($plan, $instrucciones) con $hilos hilos en $imagen ********************************"
+                                OMP_SCHEDULE="$plan,$instrucciones"
+
+                                OMP_NUM_THREADS=$hilos
+                                echo
+
+                                if [ $imagen = "Libro1.pgm" ]; then
+                                ./app "$imagen" 177
+                                ./app "$imagen" 177
+                                ./app "$imagen" 177
+                                fi
+                                if [ $imagen = "Libro2.pgm" ]; then
+                                ./app "$imagen" 177.5
+                                ./app "$imagen" 177.5
+                                ./app "$imagen" 177.5
+                                fi
+                                if [ $imagen = "Libro3.pgm" ]; then
+                                ./app "$imagen" 125
+                                ./app "$imagen" 125
+                                ./app "$imagen" 125
+                                fi
+                                echo
+                        done
+                done
+        done
+done
---
+++ shellscript/script_paralela.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+#BUCLE PARA RECORRER LAS TRES IMAGENES
+for imagen in Libro1.pgm Libro2.pgm Libro3.pgm
+do
+        #!BUCLE PARA VARIAR LA PLANIFICACION
+        for plan in static dynamic
+        do
+                #BUCLE PARA VARIAR EL NUMERO DE INSTRUCCIONES
+                for bloque in 0 1 8 32
+                do
+                        #BUCLE PARA VARIAR EL NUMERO DE HILOS
+                        for hilos in 2 4 8 16 32
+                        do
+                                echo
+                                echo "******************************** ($plan, $bloque) con $hilos hilos en $imagen ********************************"
+                                export OMP_SCHEDULE="$plan,$bloque"
+
+                                export OMP_NUM_THREADS=$hilos
+                                echo
+
+                                if [ $imagen = "Libro1.pgm" ]; then
+                                ./app "$imagen" 177
+                                fi
+                                if [ $imagen = "Libro2.pgm" ]; then
+                                ./app "$imagen" 177.5
+                                fi
+                                if [ $imagen = "Libro3.pgm" ]; then
+                                ./app "$imagen" 125
+                                fi
+                                echo
+                        done
+                done
+        done
+done
